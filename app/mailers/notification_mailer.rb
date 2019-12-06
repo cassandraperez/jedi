c8b8e9a413931ed065e182ceb9d6ab9f358d5d16
Cassandra Perez <cassie.perez6@icloud.com>
2019-12-06 19:10:27 +0000

    email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,11 @@
 class NotificationMailer < ApplicationMailer
 	default from: "coding1022@gmail.com"
 
-	def comment_added
-		mail(to: "cassandrataylorperez@gmail.com",
-			 subject: "A comment has been added to your place")
+	def comment_added(comment)
+		@place = comment.place
+		@place_ownder = @place.user
+
+		mail(to: @place_owner.email,
+			 subject: "A comment has been added to #{@place.name}")
 	end
 end
